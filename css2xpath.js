7c44ab4e6d7cfad2bbe6ba06578773193652134c
harryovers <harryovers@gmail.com>
2013-09-24 17:08:22 +0100

    moved some of the info from the comments in the js file to the readme file

--- css2xpath.js
+++ css2xpath.js
@@ -2,13 +2,6 @@
  * ---
  * this script is based on the work of James Padolsey
  * ---
- * Has been tested on the following selectors:
- * CSS2XPATH('div a');                        //  =>  //div//a
- * CSS2XPATH('div#intro a.active');           //  =>  //div[@id='intro']//a[contains(concat(' ',@class,' '),' active ')]
- * CSS2XPATH('#apple pear.lemon');            //  =>  //*[@id='apple']//pear[contains(concat(' ',@class,' '),' lemon ')]
- * CSS2XPATH('ul.nav li:first-child a');      //  =>  //ul[contains(concat(' ',@class,' '),' nav ')]//li[1]//a
- * CSS2XPATH('body > div > p:nth-child(3)');  //  =>  //body/div/p[3]
- * CSS2XPATH('li > input[type="text"]');      //  =>  //li/input[@type="text"]
  */
  
 function CSS2XPATH(selector) {
